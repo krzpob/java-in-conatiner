388c3600c7f85e9356ef84d963f1deee3b6974ca
Krzysztof Pobożan <krzpob@gmail.com>
2019-10-17 22:38:34 +0200

    kontroller

--- users-app/src/main/java/com/example/users/controllers/UsersController.java
+++ users-app/src/main/java/com/example/users/controllers/UsersController.java
@@ -1,9 +1,42 @@
 package com.example.users.controllers;
 
+import javax.validation.Valid;
+import java.util.List;
+
+import com.example.users.exceptions.ResourceNotFoundException;
+import com.example.users.models.User;
+import com.example.users.repositories.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 class UsersController{
 
+    private final UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Integer id){
+        return userRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+    }
 
 }
